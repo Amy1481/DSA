c044e00445943d705374a90a43562baeea533fa8
Amisha mohanty <amishamohanty2025@gmail.com>
2025-12-12 00:00:33 +0530

    Add files via upload

---
+++ Java/Basics/Q2.java
@@ -0,0 +1,9 @@
+// WAP to calculate the area of a rectangle
+public class Q2 {
+    public static void main(String[] args) {
+        double radius=7.8;
+        double height=12.4;
+        double area = 0.5*height*radius;
+        System.out.println("The area of the rectangle is: "+area);
+    }
+}
