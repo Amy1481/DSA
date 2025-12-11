b5717252d074d479a84ffde159ade707025fbbb9
Amisha mohanty <amishamohanty2025@gmail.com>
2025-12-12 00:00:14 +0530

    Add files via upload

---
+++ Java/Basics/Q1.java
@@ -0,0 +1,9 @@
+// WAP to print the mailing address of your home
+public class Q1{
+    public static void main(String[] args) {
+        System.out.println("Amisha Mohanty");
+        System.out.println("Kalinga Vihar");
+        System.out.println("Khordha");
+        System.out.println("BBSR");
+    }
+}
